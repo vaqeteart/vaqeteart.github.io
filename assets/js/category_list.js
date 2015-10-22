7418abec7c1a1f402cd6eb7c3e86a9464bf69bca
Miracle <miracle.lv@tpv-tech.com>
2015-10-22 17:38:28 +0800

    improve categories and sub categories display.

--- assets/js/category_list.js
+++ assets/js/category_list.js
@@ -82,64 +82,6 @@ function create_categories(category, level, countmap)
         line = "";
     }
 }
-function create_categories2(category, level)
-{
-    var line = "";
-    if (level > 0)
-    {
-        /*<li id="xxx" class="categories_item_tree">*/
-        line += "<li id='" + category.id  + "' class='categories_item_tree'>";
-        if(category.children != null){
-            /*<span id="categories_note_switch" class="tree_switch" onclick="switch_fold('categories_note')">▷</span>*/
-            line += "<span id='" + category.id 
-            + "_switch' class='tree_switch' onclick=\"switch_fold('"
-            + category.id + "')\">▷</span>";
-        }
-
-
-        /*<a href="/categories/xxx" >*/ 
-        line += "<a href='" + category.path + "'>";
-
-        /*<span id="categories_note_title">记事</span>*/
-        line += "<span id='" + category.id +"_title'>" + category.title 
-        + "</span>";
-
-        /*<span id="categories_note_count">({{site.categories.note|size}})</span>*/
-        line += "<span id='" + category.id + "_count'>(" + ")</span>";
-        line += "</a>";
-    }
-    else {
-        /*<ul id="xxx_ul" class="categories_level0" type="none">*/
-        line += "<ul id='" + category.id + "_ul' class='categories_level"
-        + level + "' type='none'>";
-    }
-    
-    if(category.children == null)
-    {
-        /*</li>*/
-        line += "</id>";
-    }
-    else {
-        if (level > 0)
-        {
-            /*<ul id="categories_note_ul" class="categories_level1" style="display:none;">*/
-            line += "<ul id='" + category.id + "_ul' class='categories_level"
-            + level + "' type='none' style=display:none;'>";
-        }
-        document.write(line);
-        line = "";
-        for(var key in category.children)
-        {
-            create_categories(category.children[key], level+1);
-        }
-
-        /*</ul>*/
-        line += "</ul>";
-        /*</li>*/
-        line += "</id>";
-    }
-    document.write(line);
-}
 
 function switch_fold(id)
 {
@@ -185,24 +127,40 @@ function change_category(id)
     //home_state = "category"
 }
 
-function change_tag(id)
+function change_categories(cats)
 {
-    header1 = document.getElementById('header1');
-    header1.innerHTML = "标签："+id;
-
-    posts = document.getElementById('post-list');
-    //alert(id)
-    //{% for post in site.posts[page.category] %}
-    //    alert({{ 'post._name' }});
-    //{% endfor %}
-    //children = posts.childNodes;
-    //for (child in children)
-    //{
-    //    child.getElementById
-    //}
-    if (e != null)
+    cats = cats.split("_");
+    var ids = new Array(cats.length);
+    
+    for (i = 0; i < cats.length; ++i)
     {
-        e.style.display = "none";
+	var path = new Array(i+1);
+
+        for (j = 0; j < path.length; ++j)
+        {
+	    path[j] = cats[j];
+	}
+	
+        ids[i] = path.join("_");
     }
-    //home_state = "category"
+
+    for (i = 0; i < ids.length; ++i)
+    {
+        e = document.getElementById(ids[i]);
+        element_switch=document.getElementById(ids[i]+"_switch");
+        element_ul=document.getElementById(ids[i]+"_ul");
+        if (e != null)
+        {
+            element_ul.style.display='block';
+            //element_switch.getElementsByTagName("img")[0].src="/assets/icons/blacks/16x16/br_down.png";
+	    if (element_switch.innerHTML=="▷")
+	    {
+		element_switch.innerHTML="▽";
+	    }
+	    else if (element_switch.innerHTML=="○")
+	    {
+		element_switch.innerHTML="●";//??
+	    }
+        }
+    }
 }
