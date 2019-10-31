a6d3537c2ce7b03b22903c187bbf13d8a5f6fef1
Miracle Lv <miracle.lv@tpv-tech.com>
2019-10-31 17:36:42 +0800

    Hexo Site updated.

--- js/common.js
+++ js/common.js
@@ -19,7 +19,7 @@ function getQuery(name) {
 
 //  <body onload="loadContent('<%= page.title %>');">
 function loadContent(title) {
-    if ("categories" == title) {
+    if ("about" != title) {
         //var category_info = getCategoriesInfo();
         //var td = document.getElementById("td_posts");
         //        var textNode = document.createTextNode(JSON.stringify(category_info.categories));
@@ -30,3 +30,53 @@ function loadContent(title) {
         ;
     }
 }
+
+$(document).ready(function () {
+    var navitm_over = 0;
+    $('.navbar th.navitm').mouseenter(function () {
+        ++navitm_over;
+        //alert('hello!');
+
+        if (1 == navitm_over) {
+            var ul_tree = 0;
+            $('.second', this).fadeIn("slow");
+            $('#full_categories_tree ul').hide();
+            $('ul.categories_tree li.categories_tree_item').mouseenter(function () {
+                ++ul_tree;
+                if (1 == ul_tree) {
+                    //alert($('a:hover', this).html());
+                    if ($('span.tree_switch:first', this).html() == "▷") {
+                        $('span.tree_switch:first', this).html("▽");
+                        $('ul:first', this).slideDown("slow");
+                    }
+                }
+            });
+
+            $('ul.categories_tree li.categories_tree_item').delay(1000).mouseleave(function () {
+                ul_tree = 0;
+                /*
+                if ($('span.tree_switch:first', this).html() == "▽") {
+                    $('ul:first', this).delay(300).slideUp("slow");
+                    $('span.tree_switch:first', this).html("▷");
+                }
+                */
+            });
+        }
+    });
+
+    $('.navbar th.navitm').mouseleave(function () {
+        navitm_over = 0;
+
+        if ($('span.tree_switch', this).html() == "▽") {
+            $('span.tree_switch', this).html("▷");
+        }
+
+        $('.second', this).delay(2000).fadeOut("slow");
+    });
+
+    if ("about" == document.title) {
+        /*var index=0;
+        var word=document.getElementById("w").innerHTML;
+        setInterval(type(word,index), 200);*/
+    }
+});
